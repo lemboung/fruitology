de8fd1ca054ec2000c2e31d2ba38c130238e2f19
lemboung <lemboung@hotmail.com>
2016-05-05 10:42:39 +0700

    change base_url

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:81/fruitology';
+$config['base_url'] = 'http://fruitology.azurewebsites.com';
 
 /*
 |--------------------------------------------------------------------------
