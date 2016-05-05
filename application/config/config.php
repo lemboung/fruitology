84269be6df4fa0b93d938db936904f6fc76d8875
lemboung <lemboung@hotmail.com>
2016-05-05 12:12:21 +0700

    change web config file

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://fruitology.azurewebsites.com/site/wwwroot/';
+$config['base_url'] = 'http://fruitology.azurewebsites.com';
 
 
 /*
